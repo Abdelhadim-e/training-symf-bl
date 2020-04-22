9be5e4772243339f866b7f1fbf7b2dc95c802e12
Abdelhadim <e.abdelhadim@gmail.com>
2020-04-22 11:40:37 +0200

    add id url article | btn

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -33,10 +33,16 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/blog/12", name="blog_show")
+     * @Route("/blog/{id}", name="blog_show")
      **/
-    public function show(){
-        return $this->render('blog/show.html.twig');
+    public function show($id){
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+
+        $article = $repo->find($id);
+
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+        ]);
     }
     
 }
